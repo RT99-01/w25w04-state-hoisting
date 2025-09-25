2dbcb546dc22d401673b5db3aa70e0121ce9168c
RT99 <kang67999@gmail.com>
2025-09-25 13:41:22 +0900

    💩카운터 2개

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,23 @@ import './App.css'
 import { useState } from 'react'
 
 function App() {
-  const [count1, setCount1] = useState(0)
-  const [count2, setCount2] = useState(0)
-
   return (
     <div>
-      <Counter count={count1} onIncrement={() => setCount1(prev => prev + 1)} />
-      <Counter count={count2} onIncrement={() => setCount2(prev => prev + 1)} />
+      <Counter/>
+      <Counter/>
     </div>
   )
 }
 
-function Counter({ count, onIncrement }) {
+function Counter() {
+  const [count, setCount] = useState(0)
+
   return (
     <div>
       <h1>Counter: {count}</h1>
-      <button onClick={onIncrement}>
-        증가
+      <button 
+        onClick={ () => setCount( prev => prev + 1 ) }>
+          증가
       </button>
     </div>
   )
